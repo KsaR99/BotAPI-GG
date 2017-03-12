75bc418138f1ee08134b9281ac704d3effb7b135
KsaR <KsaR99@users.noreply.github.com>
2017-03-12 06:44:19 +0100

    Update © since

--- PushConnection.php
+++ PushConnection.php
@@ -1,22 +1,23 @@
 <?php
+
 /**
-Biblioteka implementująca BotAPI GG <https://boty.gg.pl>
-Copyright (C) 2013 GG Network S.A. Marcin Bagiński <marcin.baginski@firma.gg.pl>
-Modified by KsaR 2016 <https://github.com/KsaR99/>
-
-This library is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-See the GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>
-**/
+ * Biblioteka implementująca BotAPI GG <https://boty.gg.pl>
+ * Copyright (C) 2013-2016 Xevin Consulting Limited Marcin Bagiński <marcin.baginski@firma.gg.pl>
+ * Modified by KsaR 2017 <https://github.com/KsaR99/>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ */
 
 require_once __DIR__.'/MessageBuilder.php';
 
