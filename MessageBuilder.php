28dbb3e29ad8284133614d138070f2fe89a45b04
KsaR <KsaR99@users.noreply.github.com>
2017-03-23 12:38:24 +0100

    Update MessageBuilder.php

--- MessageBuilder.php
+++ MessageBuilder.php
@@ -3,7 +3,7 @@
 /**
  * Biblioteka implementująca BotAPI GG <https://boty.gg.pl>
  * Copyright (C) 2013-2016 Xevin Consulting Limited Marcin Bagiński <marcin.baginski@firma.gg.pl>
- * Modified by KsaR 2017 <https://github.com/KsaR99/>
+ * Modified by KsaR 2016-2017 <https://github.com/KsaR99/>
  *
  * This library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,16 +19,9 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>
  */
 
-define('BOTAPI_VERSION', 'GGBotApi-2.5-PHP');
-
-define('FORMAT_NONE', 0x00);
-define('FORMAT_BOLD_TEXT', 0x01);
-define('FORMAT_ITALIC_TEXT', 0x02);
-define('FORMAT_UNDERLINE_TEXT', 0x04);
-define('FORMAT_NEW_LINE', 0x08);
-
-define('IMG_FILE', true);
-define('IMG_RAW', false);
+const BOTAPI_VERSION = 'GGBotAPI-3.0;PHP-'.PHP_VERSION;
+const IMG_FILE = true;
+const IMG_RAW = false;
 
 require_once __DIR__.'/PushConnection.php';
 
@@ -37,283 +30,171 @@ require_once __DIR__.'/PushConnection.php';
  */
 class MessageBuilder
 {
-		/**
-		 * Tablica numerów GG do których ma trafić wiadomość
-		 */
-		public $recipientNumbers = [];
+    public $recipientNumbers = [];
 
-		/**
-		 * Określa czy wiadomość zostanie wysłana do numerów będących offline, domyślnie true
-		 */
-		public $sendToOffline = true;
+    public $html = '';
+    public $text = '';
+    public $format = null;
 
-		public $html = '';
-		public $text = '';
-		public $format = '';
+    /**
+     * Konstruktor MessageBuilder
+     */
+    public function __construct()
+    {
+    }
 
-		public $rgb = [0, 0, 0];
+    /**
+     * Czyści całą wiadomość
+     */
+    public function clear()
+    {
+        $this->recipientNumbers = [];
 
-		/**
-		 * Konstruktor MessageBuilder
-		 */
-		public function __construct()
-		{
-		}
+        $this->html = '';
+        $this->text = '';
+        $this->format = null;
+    }
 
-		/**
-		 * Czyści całą wiadomość
-		 */
-		public function clear()
-		{
-				$this->recipientNumbers = [];
-				$this->sendToOffline = true;
+    /**
+     * Dodaje tekst do wiadomości
+     *
+     * @param string $text tekst do wysłania
+     *
+     * @return MessageBuilder this
+     */
+    public function addText(string $text)
+    {
+        $text = str_replace(["\n", "\r\r"], ["\r\n", "\r"], $text);
+        $html = str_replace("\r\n", '<br>', htmlspecialchars($text, ENT_NOQUOTES, 'UTF-8'));
 
-				$this->html = '';
-				$this->text = '';
-				$this->format = '';
+        if ($this->format !== null) {
+            $this->text.= $text;
+        }
 
-				$this->rgb = [0, 0, 0];
-		}
+        $this->html.= $html;
 
-		/**
-		 * Dodaje tekst do wiadomości
-		 *
-		 * @param string $text tekst do wysłania
-		 * @param int $formatBits styl wiadomości (FORMAT_*_TEXT), domyślnie brak
-		 * @param int $R, $G, $B składowe koloru tekstu w formacie RGB
-		 *
-		 * @return MessageBuilder this
-		 */
-		public function addText($text, $formatBits = FORMAT_NONE, $R = 0, $G = 0, $B = 0)
-		{
-				if ($formatBits & FORMAT_NEW_LINE) {
-						$text.= "\r\n";
-				}
+        return $this;
+    }
 
-				$text = str_replace(["\n", "\r\r"], ["\r\n", "\r"], $text);
-				$html = str_replace("\r\n", '<br>', htmlspecialchars($text, ENT_NOQUOTES, 'UTF-8'));
+    /**
+     * Dodaje tekst do wiadomości
+     *
+     * @param string $html tekst do wysłania w HTMLu
+     *
+     * @return MessageBuilder this
+     */
+    public function addRawHtml(string $html)
+    {
+        $this->html.= $html;
 
-				if ($this->format !== null) {
-						$rgb = &$this->rgb;
+        return $this;
+    }
 
-						$this->format.= pack('vC', mb_strlen($this->text, 'UTF-8'),
-								($formatBits & FORMAT_BOLD_TEXT) |
-								($formatBits & FORMAT_ITALIC_TEXT) |
-								($formatBits & FORMAT_UNDERLINE_TEXT) |
-								(1 || $R != $rgb[0] || $G != $rgb[1] || $B != $rgb[2]) * 0x08).
-						pack('CCC', $R, $G, $B);
+    /**
+     * Ustawia tekst do wiadomości
+     *
+     * @param string $html tekst do wysłania w HTMLu
+     *
+     * @return MessageBuilder this
+     */
+    public function setRawHtml(string $html)
+    {
+        $this->html = $html;
 
-						$rgb = [$R, $G, $B];
-						$this->text.= $text;
-				}
-				if ($R || $G || $B) {
-						$html = '<span style="color:#'.substr('00'.dechex($R), -2).substr('00'.dechex($G), -2).substr('00'.dechex($B), -2)."\">$html</span>";
-				}
-				if ($formatBits & FORMAT_BOLD_TEXT) {
-						$html = '<b>'.$html.'</b>';
-				}
-				if ($formatBits & FORMAT_ITALIC_TEXT) {
-						$html = '<i>'.$html.'</i>';
-				}
-				if ($formatBits & FORMAT_UNDERLINE_TEXT) {
-						$html = '<u>'.$html.'</u>';
-				}
+        return $this;
+    }
 
-				$this->html.= $html;
+    /**
+     * Ustawia tekst wiadomości alternatywnej
+     *
+     * @param string $message tekst do wysłania dla GG 7.7 i starszych
+     *
+     * @return MessageBuilder this
+     */
+    public function setAlternativeText(string $message)
+    {
+        $this->format = null;
+        $this->text = $message;
 
-				return $this;
-		}
+        return $this;
+    }
 
-		/**
-		 * Dodaje tekst do wiadomości
-		 *
-		 * @param string $bbcode tekst do wysłania w formacie BBCode
-		 *
-		 * @return MessageBuilder this
-		 */
-		public function addBBcode($bbcode)
-		{
-				$tagsLength = $start = 0;
-				$heap = [];
-				$bbcode = str_replace('[br]', "\n", $bbcode);
-				$regexp = '~\[(/)?(b|i|u|color)(=#?[0-9a-fA-F]{6})?\]~';
+    /**
+     * Dodaje obraz do wiadomości
+     *
+     * @param string $path ścieżka do pliku graficznego
+     *
+     * @return MessageBuilder this
+     */
+    public function addImage(string $path, $isFile = IMG_FILE)
+    {
+        if (empty(PushConnection::$lastAuthorization)) {
+            throw new Exception('Użyj klasy PushConnection');
+        }
 
-				while (preg_match($regexp, $bbcode, $out, PREG_OFFSET_CAPTURE, $start)) {
-						$s = substr($bbcode, $start, $out[0][1]-$start);
-						$c = [0, 0, 0];
+        $content = '';
 
-						if (!empty($s)) {
-								$flags = 0;
+        if ($isFile) {
+            $content.= file_get_contents($path);
+        }
 
-								foreach ($heap as $h) {
-										switch ($h[0]) {
-												case 'b': $flags|= 0x01; break;
-												case 'i': $flags|= 0x02; break;
-												case 'u': $flags|= 0x04; break;
-												case 'color': $c = $h[1]; break;
-										}
-								}
+        $crc = crc32($content);
+        $contentLength = strlen($content);
+        $hash = sprintf('%08x%08x', $crc, $contentLength);
 
-								$this->addText($s, $flags, $c[0], $c[1], $c[2]);
-						}
+        $p = new PushConnection;
 
-						if ($out[1][0] === '') {
-								if (in_array($out[2][0], ['b', 'i', 'u'])) {
-										$heap[] = [$out[2][0]];
-								} elseif ($out[2][0] === 'color') {
-										$c = hexdec(substr($out[3][0], -6));
-										$heap[] = ['color', [($c>>16) & 0xFF, ($c>>8) & 0xFF, ($c>>0) & 0xFF]];
-								}
-						} else {
-								array_pop($heap);
-						}
+        if (!$p->existsImage($hash) && !$p->putImage($content)) {
+            throw new Exception('Nie udało się wysłać obrazka');
+        }
 
-						$tagsLength += $tagLen = strlen($out[0][0]);
-						$start = $out[0][1]+$tagLen;
-				}
+        $this->format.= pack('vCCCVV', strlen($this->text), 0x80, 0x09, 0x01, $contentLength, $crc);
+        $this->addRawHtml('<img name="'.$hash.'">');
 
-				$s = substr($bbcode, $start);
+        return $this;
+    }
 
-				if (!empty($s)) {
-						$this->addText($s);
-				}
+    /**
+     * Ustawia odbiorców wiadomości
+     *
+     * @param int|string|array $recipientNumbers GG odbiorców
+     *
+     * @return MessageBuilder this
+     */
+    public function setRecipients($recipientNumbers)
+    {
+        $this->recipientNumbers = (array)$recipientNumbers;
 
-				return $this;
-		}
+        return $this;
+    }
 
-		/**
-		 * Dodaje tekst do wiadomości
-		 *
-		 * @param string $html tekst do wysłania w HTMLu
-		 *
-		 * @return MessageBuilder this
-		 */
-		public function addRawHtml($html)
-		{
-				$this->html.= $html;
+    /**
+     * Tworzy sformatowaną wiadomość do wysłania do BotMastera
+     */
+    public function getProtocolMessage()
+    {
+        $formatLen = strlen($this->format);
 
-				return $this;
-		}
+        return pack('VVVV',
+        strlen($this->html)+1,
+        strlen($this->text)+1,
+        0,
+          (empty($this->format) ? 0 : $formatLen+3)).$this->html."\0$this->text\0".
+          (empty($this->format) ? '' : pack('Cv', 0x02, $formatLen).$this->format
+        );
+    }
 
-		/**
-		 * Ustawia tekst do wiadomości
-		 *
-		 * @param string $html tekst do wysłania w HTMLu
-		 *
-		 * @return MessageBuilder this
-		 */
-		public function setRawHtml($html)
-		{
-				$this->html = $html;
+    /**
+     * Zwraca na wyjście sformatowaną wiadomość do wysłania do BotMastera
+     */
+    public function reply()
+    {
+        if (!empty($this->recipientNumbers)) {
+            header('To: '.implode(',', $this->recipientNumbers));
+        }
 
-				return $this;
-		}
+        header('BotApi-Version: '.BOTAPI_VERSION);
 
-		/**
-		 * Ustawia tekst wiadomości alternatywnej
-		 *
-		 * @param string $message tekst do wysłania dla GG 7.7 i starszych
-		 *
-		 * @return MessageBuilder this
-		 */
-		public function setAlternativeText($message)
-		{
-				$this->format = null;
-				$this->text = $message;
-
-				return $this;
-		}
-
-		/**
-		 * Dodaje obraz do wiadomości
-		 *
-		 * @param string $path ścieżka do pliku graficznego
-		 *
-		 * @return MessageBuilder this
-		 */
-		public function addImage($path, $isFile = IMG_FILE)
-		{
-				if (empty(PushConnection::$lastAuthorization)) {
-						throw new Exception('Użyj klasy PushConnection');
-				}
-				if ($isFile == IMG_FILE) {
-						$content = file_get_contents($path);
-				}
-
-				$crc = crc32($content);
-				$fileLen = strlen($content);
-				$hash = sprintf('%08x%08x', $crc, $fileLen);
-				$p = new PushConnection;
-
-				if (!$p->existsImage($hash) && !$p->putImage($content)) {
-						throw new Exception('Nie udało się wysłać obrazka');
-				}
-
-				$this->format.= pack('vCCCVV', strlen($this->text), 0x80, 0x09, 0x01, $fileLen, $crc);
-				$this->addRawHtml('<img name="'.$hash.'">');
-
-				return $this;
-		}
-
-		/**
-		 * Ustawia odbiorców wiadomości
-		 *
-		 * @param int|string|array $recipientNumbers numer/y GG odbiorców
-		 *
-		 * @return MessageBuilder this
-		 */
-		public function setRecipients($recipientNumbers)
-		{
-				$this->recipientNumbers = (array)$recipientNumbers;
-
-				return $this;
-		}
-
-		/**
-		 * Ustawia czy dostarczyć do OFFLINE
-		 *
-		 * @param $sendToOffline bool|int
-		 *
-		 * @return MessageBuilder this
-		 */
-		public function setSendToOffline($sendToOffline)
-		{
-				$this->sendToOffline = $sendToOffline;
-
-				return $this;
-		}
-
-		/**
-		 * Tworzy sformatowaną wiadomość do wysłania do BotMastera
-		 */
-		public function getProtocolMessage()
-		{
-				if (!preg_match('#^<span[^>]*>.+</span>$#s', $this->html, $o) || isset($o[0]) && $o[0] != $this->html) {
-						$this->html = '<span style="color:#000000;font-family:\'MS Shell Dlg 2\';font-size:9pt">'.$this->html.'</span>';
-				}
-
-				$formatLen = strlen($this->format);
-
-				return pack('VVVV', strlen($this->html)+1, strlen($this->text)+1, 0,
-						(empty($this->format) ? 0 : $formatLen+3)).$this->html."\0$this->text\0".
-						(empty($this->format) ? '' : pack('Cv', 0x02, $formatLen).$this->format
-				);
-		}
-
-		/**
-		 * Zwraca na wyjście sformatowaną wiadomość do wysłania do BotMastera
-		 */
-		public function reply()
-		{
-				if (!empty($this->recipientNumbers)) {
-						header('To: '.implode(',', $this->recipientNumbers));
-				}
-				if (!$this->sendToOffline) {
-						header('Send-to-offline: 0');
-				}
-
-				header('BotApi-Version: '.BOTAPI_VERSION);
-
-				echo $this->getProtocolMessage();
-		}
+        echo $this->getProtocolMessage();
+    }
 }
