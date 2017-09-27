8118279bda2fd59ea0daef2c6e6dde3aff6e7a4f
KsaR <KsaR99@users.noreply.github.com>
2017-09-27 04:12:33 +0200

    Wydzielenie klasy BotAPIAuthorization do oddzielnego pliku.

--- libs/PushConnection.php
+++ libs/PushConnection.php
@@ -66,6 +66,7 @@ class PushConnection
             if ($userName === null && self::$BOTAPI_LOGIN !== null) {
                 $userName = self::$BOTAPI_LOGIN;
             }
+
             if ($password === null && self::$BOTAPI_PASSWORD !== null) {
                 $password = self::$BOTAPI_PASSWORD;
             }
@@ -88,6 +89,7 @@ class PushConnection
         if (!$this->authorization->isAuthorized()) {
             return false;
         }
+
         if (!is_array($messages)) {
             $messages = [$messages];
         }
@@ -96,9 +98,11 @@ class PushConnection
 
         foreach ($messages as $message) {
             $ch = $this->getSingleCurlHandle();
+
             curl_setopt_array($ch, [
                 CURLOPT_URL => 'https://'.$data['server'].'/sendMessage/'.$this->gg,
-                CURLOPT_POSTFIELDS => 'msg='.urlencode($message->getProtocolMessage()).'&to='.implode(',', $message->recipientNumbers),
+                CURLOPT_POSTFIELDS => 'msg='.urlencode($message->getProtocolMessage())
+                                    . '&to='.implode(',', (array)$message->recipientNumbers),
                 CURLOPT_HEADER => false,
                 CURLOPT_HTTPHEADER => [
                     'BotApi-Version: '.MessageBuilder::BOTAPI_VERSION,
@@ -106,6 +110,7 @@ class PushConnection
             ]]);
 
             $r = curl_exec($ch);
+
             curl_close($ch);
 
             if (strpos($r, '<status>0</status>') === false) {
@@ -149,6 +154,7 @@ class PushConnection
         ]);
 
         $res = curl_exec($ch);
+
         curl_close($ch);
 
         if (strpos($res, '<status>0</status>') === false) {
@@ -200,6 +206,7 @@ class PushConnection
         ]);
 
         $r = curl_exec($ch);
+
         curl_close($ch);
 
         return $r;
@@ -245,6 +252,7 @@ class PushConnection
         }
 
         $ch = $this->getSingleCurlHandle();
+
         curl_setopt_array($ch, [
             CURLOPT_URL => 'https://botapi.gadu-gadu.pl/botmaster/isBot/'.$this->gg,
             CURLOPT_POSTFIELDS => 'check_ggid='.$ggid,
@@ -259,73 +267,5 @@ class PushConnection
         curl_close($ch);
 
         return strpos($r, '<status>1</status>') !== false;
-    }
-}
-
-/**
- * Pomocnicza klasa do autoryzacji przez HTTP
- */
-class BotAPIAuthorization
-{
-    private $data = [
-        'token' => null,
-        'server' => null,
-        'port' => null
-    ];
-
-    private $isValid;
-
-    /**
-     * @return bool true jeśli autoryzacja przebiegła prawidłowo
-     */
-    public function isAuthorized()
-    {
-        return $this->isValid;
-    }
-
-    public function __construct($ggid, $userName, $password)
-    {
-        $this->isValid = $this->getData($ggid, $userName, $password);
-    }
-
-    private function getData($ggid, $userName, $password)
-    {
-        $ch = curl_init();
-        curl_setopt_array($ch, [
-            CURLOPT_URL => 'https://botapi.gadu-gadu.pl/botmaster/getToken/'.$ggid,
-            CURLOPT_USERPWD => $userName.':'.$password,
-            CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_SSL_VERIFYPEER => false,
-            CURLOPT_VERBOSE => PushConnection::CURL_VERBOSE,
-            CURLOPT_HTTPHEADER => ['BotApi-Version: '.MessageBuilder::BOTAPI_VERSION],
-        ]);
-
-        $xml = curl_exec($ch);
-        curl_close($ch);
-
-        $regexp = '~<token>(.+?)</token><server>(.+?)</server><port>(\d+)</port>~';
-
-        if (!preg_match($regexp, $xml, $out)) {
-            return false;
-        }
-
-        $this->data = [
-            'token' => $out[1],
-            'server' => $out[2],
-            'port' => $out[3]
-        ];
-
-        return true;
-    }
-
-    /**
-     * Pobiera aktywny token, port i adres BotMastera
-     *
-     * @return array
-     */
-    public function getServerAndToken()
-    {
-        return $this->data;
     }
 }
