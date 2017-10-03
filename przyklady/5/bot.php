c77327fde76421e9c332a2a3da33721a479ae3c2
KsaR <KsaR99@users.noreply.github.com>
2017-10-03 02:27:18 +0200

    Create bot.php

---
+++ przyklady/5/bot.php
@@ -0,0 +1,8 @@
+<?php
+
+spl_autoload_register(function($class) {
+    include __DIR__."/../libs/{$class}.php";
+});
+
+$p = new PushConnection(123456, 'wojtek@gg.pl', 'hasło'); // autoryzacja
+$p->setStatus('Mój nowy opis', PushConnection::STATUS_AWAY);
