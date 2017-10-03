9fba6edd8220f2bedeaa9ac9f8f8834bdc53b19d
KsaR <KsaR99@users.noreply.github.com>
2017-10-03 02:25:49 +0200

    Create bot.php

---
+++ przyklady/3/bot.php
@@ -0,0 +1,25 @@
+<?php
+
+spl_autoload_register(function($class) {
+    include __DIR__."/../libs/{$class}.php";
+});
+
+$m = new MessageBuilder;
+
+switch (rand(1, 3)) {
+    case 1:
+        $m->addText("Pierwsza linia\nDruga linia");
+        break;
+    case 2:
+        $m
+          ->addText('Tekst wysłany do innych')
+          ->setRecipients([123, 456]);
+        break;
+    case 3:
+        $m
+          ->addText('Tekst wysłany do Ciebie i innych')
+          ->setRecipients([123, 456, $_GET['from']]);
+        break;
+}
+
+$m->reply();
